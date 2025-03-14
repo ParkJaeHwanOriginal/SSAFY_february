98218a2ac45546db08e7b0b5cf3900a5ba1e26e6
park jaehwan <pogpog1230@naver.com>
2025-03-14 17:56:28 +0900

    last

--- algorithm/250314/pool.py
+++ algorithm/250314/pool.py
@@ -5,28 +5,24 @@ t = int(input())
 for tc in range(1, t+1) :
     day, month, thr_month, year = map(int, input().split())
     lst = list(map(int,input().split()))
-    answer = 0
-    for i in range(len(lst)) :                  # 일권 or 월권
-        if lst[i] * 10 > month :
+
+    # 일권 비용으로 변경
+    for i in range(len(lst)) :
+        if lst[i] * day > month :
             lst[i] = month
-            answer += month
         else :
-            answer += 10 * lst[i]
+            lst[i] *= day
+    print(lst)
+
+    answer = 0
+    for i in range(len(lst)) :
+        if i == 1 or i == 2 :                                       # 월요금만 더하는 answer와 3달요금을 계산해준 answer를 나눠서 비교하면서 가야할 듯
+            answer += lst[i]
+
+        elif i >= 3 :
+            if answer - (lst[i-2] + lst[i-1]) + month > answer + lst[i] : # 앞전 2달을 포함한 3개월을 3달요금으로 내는게 각달요금보다 싸다면
+                answer = answer - lst[i-2] - lst[i-1] + month
+            else :
+                answer += thr_month
     print(answer)
 
-    if year < answer :                          # 모두 연권으로
-        answer = year
-
-    # temp_answer = 0
-    # for i in range(len(lst)) :
-    #     if i < 10 :
-    #         if
-    # for i in lst :
-    #     if i*10 > month :
-    #         temp_answer += month
-    #     else :
-    #         temp_answer += i*10
-    # if temp_answer < answer :
-    #     answer = temp_answer
-
-    print(answer)
